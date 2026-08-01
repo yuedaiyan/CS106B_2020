2d03bf4c7313087fb813fd46e3398ed76bbce795
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-08-02 01:33:10 +0800

    course: lecture 13 start

---
+++ lecture13_inClass/Lecture13Starter/src/juryselection.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "vector.h"
+#include "testing/SimpleTest.h"
+
+using namespace std;
+
+struct juror {
+    string name;
+    int bias;
+};
+
+void displayJury(Vector<juror>& jury){
+    for (juror j: jury) {
+        cout << "(" << j.name << " " << j.bias << ") ";
+    }
+    cout << endl;
+}
+
+
+void findAllUnbiasedJuries(Vector<juror>& allCandidates){
+    /* TODO: Fill me in! */
+}
+
+
+PROVIDED_TEST("Show jury selection process"){
+    cout << endl;
+    Vector<juror> allCandidates = {{"Nick", -3}, {"Kylie", 3}, {"Trip", 0}};
+    findAllUnbiasedJuries(allCandidates);
+}
