e734b3c4dac7e65fb61a663f99494697c11f59fd
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-08-01 15:39:03 +0800

    add: lecture12 start files

---
+++ lecture12_inClass/Lecture12/src/permutations.cpp
@@ -0,0 +1,10 @@
+#include "vector.h"
+#include "testing/SimpleTest.h"
+#include "random.h"
+
+using namespace std;
+
+void listPermutations(string s){
+    /* TODO: Fill in this function! */
+}
+
---
+++ lecture12_inClass/Lecture12/src/prototypes.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "set.h"
+
+void listPermutations(std::string s);
+void listSubsets(Set<std::string>& choices);
