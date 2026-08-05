8596dc5562a8947476159d5a295c73b97aca0475
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-08-05 10:51:19 +0800

    course: lecture 12 finished

--- lecture_2020summer/L12_lecture07_09/lecture12_inClass/Lecture12Starter/src/subsets.cpp
+++ lecture_2020summer/L12_lecture07_09/lecture12_inClass/Lecture12Starter/src/subsets.cpp
@@ -1,9 +1,36 @@
-#include <iostream>
 #include "set.h"
+#include "testing/SimpleTest.h"
+#include <iostream>
 
 using namespace std;
 
-void listSubsets(Set<string>& choices) {
-    /* TODO: Fill in this function! */
+void listSubsetsHelp(Set<string>& choices, Set<string>& soFar) {
+    if (choices.isEmpty()) {
+        cout << soFar.toString() << endl;
+    } else {
+        string curr = choices.first();
+        choices.remove(curr);
+
+        // not include curr
+        listSubsetsHelp(choices, soFar);
+
+        // include curr
+        soFar.add(curr);
+        listSubsetsHelp(choices, soFar);
+
+        // reset
+        soFar.remove(curr);
+        choices.add(curr);
+    }
 }
 
+void listSubsets(Set<string>& choices) {
+    Set<string> soFar;
+    cout << endl;
+    listSubsetsHelp(choices, soFar);
+}
+
+STUDENT_TEST("main(): ") {
+    Set<string> trySet = { "a", "b", "c" };
+    listSubsets(trySet);
+}
