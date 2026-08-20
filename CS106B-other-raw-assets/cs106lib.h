356303f03967ae72cb5f2a9325b81ba53fe83e95
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-08-20 13:42:59 +0800

    fix: limit LFS tracking to large asset files

--- CS106B-other-raw-assets/cs106lib.h
+++ CS106B-other-raw-assets/cs106lib.h
@@ -1,3 +1,34 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:95f444a3a992e3e5a56c46bfc9aded82ed7999e130ff30e5f2ab9f6b0739db78
-size 666
+// myheaders.h
+#pragma once
+
+// Stanford CS106 Library
+#include "SimpleTest.h" // IWYU pragma: keep (needed to quiet spurious warning)
+#include "console.h"
+#include "direction.h"
+#include "error.h"
+#include "filelib.h"
+#include "grid.h"
+#include "gridlocation.h"
+#include "gtypes.h"
+#include "hashmap.h"
+#include "hashset.h"
+#include "lexicon.h"
+#include "map.h"
+#include "priorityqueue.h"
+#include "queue.h"
+#include "random.h"
+#include "set.h"
+#include "simpio.h"
+#include "stack.h"
+#include "strlib.h"
+#include "timer.h"
+#include "vector.h"
+
+// C++ Standard Library
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <cctype>
+
+using namespace std;
