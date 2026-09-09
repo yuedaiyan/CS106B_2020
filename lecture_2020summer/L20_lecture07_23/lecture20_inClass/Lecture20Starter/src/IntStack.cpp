074a197d5e19e4aa094971e220b9f2f25cc8ed77
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-09-10 02:41:05 +0800

    course: lecture 20 finished

--- lecture_2020summer/L20_lecture07_23/lecture20_inClass/Lecture20Starter/src/IntStack.cpp
+++ lecture_2020summer/L20_lecture07_23/lecture20_inClass/Lecture20Starter/src/IntStack.cpp
@@ -1,4 +1,5 @@
 #include "IntStack.h"
+#include "error.h"
 #include "testing/SimpleTest.h"
 
 IntStack::IntStack() {
@@ -6,29 +7,40 @@ IntStack::IntStack() {
 }
 
 IntStack::~IntStack() {
-    /* FILL ME IN */
+    while (!this->isEmpty()) {
+        Node* temp = this->top;
+        this->top = this->top->next;
+        delete temp;
+    }
 }
 
 void IntStack::push(int value) {
-    /* FILL ME IN */
+    Node* temp = new Node;
+    temp->data = value;
+    temp->next = this->top;
+    this->top = temp;
 }
 
 int IntStack::pop() {
-    return 0;
+    if (isEmpty()) {
+        error("Error trying to pop from an empty stack");
+    }
+    Node* temp = this->top;
+    this->top = temp->next;
+    int result = temp->data;
+    delete temp;
+    return result;
 }
 
 bool IntStack::isEmpty(){
     return top == nullptr;
 }
 
-
 PROVIDED_TEST("Reverse simple set of values"){
     IntStack stack;
-
     for (int i = 0; i < 10; i++){
         stack.push(i);
     }
-
     for (int i = 9; i >= 0; i--){
         EXPECT_EQUAL(i, stack.pop());
     }
--- lecture_2020summer/L20_lecture07_23/lecture20_inClass/Lecture20Starter/src/IntStack.h
+++ lecture_2020summer/L20_lecture07_23/lecture20_inClass/Lecture20Starter/src/IntStack.h
@@ -9,11 +9,9 @@ class IntStack {
 public:
     IntStack(); // constructor
     ~IntStack();
-    
     void push(int value);
     int pop();
     bool isEmpty();
-
 private:    
     Node* top;
 };
